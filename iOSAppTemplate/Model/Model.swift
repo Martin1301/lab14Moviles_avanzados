c0b18a9c1c8c97ef74d58f70eb72722b95bfe67e
Martin1301 <67760752+Martin1301@users.noreply.github.com>
2022-06-25 12:10:50 -0500

    first commit

---
+++ iOSAppTemplate/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  iOSAppTemplate
+//
+//  Created by MAC43 on 5/06/22.
+//
+
+import Foundation
---
+++ iOSAppTemplate/ViewModels/HomeViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  HomeViewModel.swift
+//  iOSAppTemplate
+//
+//  Created by MAC43 on 5/06/22.
+//
+
+import Foundation
+
+
